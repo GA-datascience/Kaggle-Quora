eafe1f340fe5cf50c8a5d31b5ef350c65709988f
Germayne <germayneng@hotmail.com>
2017-05-08 17:35:48 +0800

    Add files via upload

--- code-analysis/randomforest.R
+++ code-analysis/randomforest.R
@@ -1,7 +1,7 @@
 #'################################
 #' Random forest for Quora kaggle
 #'############################### 
-
+# http://stackoverflow.com/questions/33881053/how-to-get-randomforest-model-output-in-probability-using-caret
 
 #' Run the xgboost python script 4 for the split. ( the negative class re proportion) THEN
 #' export the x_train and y_train as x_train.r.csv and y_train_r.csv respectively
@@ -35,16 +35,16 @@ df$is_duplicate <- as.factor(df$is_duplicate)
 # random forest training 
 # due to memory constraint, we train in smaller sets
 # we set to smaller trees and combine them at the end. total 500 trees 
-rf_1 <- randomForest(x = df_2[,-39], y = df_2$is_duplicate, ntree = 50, data = df2)
-rf_2 <- randomForest(x = df_2[,-39], y = df_2$is_duplicate, ntree = 50, data = df2)
-rf_3 <- randomForest(x = df_2[,-39], y = df_2$is_duplicate, ntree = 50, data = df2)
-rf_4 <- randomForest(x = df_2[,-39], y = df_2$is_duplicate, ntree = 50, data = df2)
-rf_5 <- randomForest(x = df_2[,-39], y = df_2$is_duplicate, ntree = 50, data = df2)
-rf_6 <- randomForest(x = df_2[,-39], y = df_2$is_duplicate, ntree = 50, data = df2)
-rf_7 <- randomForest(x = df_2[,-39], y = df_2$is_duplicate, ntree = 50, data = df2)
-rf_8 <- randomForest(x = df_2[,-39], y = df_2$is_duplicate, ntree = 50, data = df2)
-rf_9 <- randomForest(x = df_2[,-39], y = df_2$is_duplicate, ntree = 50, data = df2)
-rf_10 <- randomForest(x = df_2[,-39], y = df_2$is_duplicate, ntree = 50, data = df2)
+rf_1 <- randomForest(x = df[,-39], y = df$is_duplicate, ntree = 50, data = df)
+rf_2 <- randomForest(x = df[,-39], y = df$is_duplicate, ntree = 50, data = df)
+rf_3 <- randomForest(x = df[,-39], y = df$is_duplicate, ntree = 50, data = df)
+rf_4 <- randomForest(x = df[,-39], y = df$is_duplicate, ntree = 50, data = df)
+rf_5 <- randomForest(x = df[,-39], y = df$is_duplicate, ntree = 50, data = df)
+rf_6 <- randomForest(x = df[,-39], y = df$is_duplicate, ntree = 50, data = df)
+rf_7 <- randomForest(x = df[,-39], y = df$is_duplicate, ntree = 50, data = df)
+rf_8 <- randomForest(x = df[,-39], y = df$is_duplicate, ntree = 50, data = df)
+rf_9 <- randomForest(x = df[,-39], y = df$is_duplicate, ntree = 50, data = df)
+rf_10 <- randomForest(x = df[,-39], y = df$is_duplicate, ntree = 50, data = df)
 
 rf_total <- combine(rf_1,rf_2,rf_3,rf_4,rf_5, rf_6, rf_7, rf_8, rf_9, rf_10) 
 
@@ -57,8 +57,9 @@ x_test <- fread('x_test.csv')
 x_test[is.na(x_test)] <- 0
 
 # run the model on the test set 
-result <- predict(rf_test, newdata = x_test)
+result <- predict(rf_test, newdata = x_test, type = "prob")
 
 # export the result 
-submission <- data.frame(is_duplicate = result, test_id = df_test[,"test_id"])
+submission <- data.frame(is_duplicate = result[,2], test_id = df_test[,"test_id"])
 write.csv(submission,'randomforest.csv',row.names = FALSE) 
+
