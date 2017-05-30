bf3624268d0efaef1deaba7b2046828ead0b723a
Alson Yap <alsonyap185@hotmail.com>
2017-05-31 02:35:37 +0800

    updated script with 3rd magic feature

--- code-analysis/xgboost2.py
+++ code-analysis/xgboost2.py
@@ -671,8 +671,58 @@ def q1_q2_intersect(row):
 x_train['q1_q2_intersect'] = train_copy.apply(q1_q2_intersect, axis=1, raw=True)
 x_test['q1_q2_intersect'] = test_copy.apply(q1_q2_intersect, axis=1, raw=True)
 
+stops = set(stopwords.words("english"))
+def word_match_share(q1, q2, stops=None):
+    q1 = str(q1).lower().split()
+    q2 = str(q2).lower().split()
+    q1words = {}
+    q2words = {}
+    for word in q1:
+        if word not in stops:
+            q1words[word] = 1
+    for word in q2:
+        if word not in stops:
+            q2words[word] = 1
+    if len(q1words) == 0 or len(q2words) == 0:
+        # The computer-generated chaff includes a few questions that are nothing but stopwords
+        return 0.
+    shared_words_in_q1 = [w for w in q1words.keys() if w in q2words]
+    shared_words_in_q2 = [w for w in q2words.keys() if w in q1words]
+    R = (len(shared_words_in_q1) + len(shared_words_in_q2))/(len(q1words) + len(q2words))
+    return R
+
+# Second dictionary for 2nd magic feature
+q_dict2 = defaultdict(dict)
+for i in range(ques.shape[0]):
+        wm = word_match_share(ques.question1[i], ques.question2[i], stops=stops)
+        q_dict2[ques.question1[i]][ques.question2[i]] = wm
+        q_dict2[ques.question2[i]][ques.question1[i]] = wm
+        
+# Function for feature        
+def q1_q2_wm_ratio(row):
+    q1 = q_dict2[row['question1']]
+    q2 = q_dict2[row['question2']]
+    inter_keys = set(q1.keys()).intersection(set(q2.keys()))
+    if(len(inter_keys) == 0): return 0.
+    inter_wm = 0.
+    total_wm = 0.
+    for q,wm in q1.items():
+        if q in inter_keys:
+            inter_wm += wm
+        total_wm += wm
+    for q,wm in q2.items():
+        if q in inter_keys:
+            inter_wm += wm
+        total_wm += wm
+    if(total_wm == 0.): return 0.
+    return inter_wm/total_wm
+
+# Generate the features for wm ratio
+x_train['q1_q2_wm_ratio'] = train_copy.apply(q1_q2_wm_ratio, axis=1, raw=True)
+x_test['q1_q2_wm_ratio'] = test_copy.apply(q1_q2_wm_ratio, axis=1, raw=True)
+
 # Delete the generated variables
-del q_dict, ques, train_copy, test_copy
+del q_dict, ques, train_copy, test_copy, q_dict2
 
 
 ################################################################################
