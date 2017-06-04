47137b575b0ba2dc16d03e3c9d4065878a034f95
Germayne <germayneng@hotmail.com>
2017-06-04 23:36:48 +0800

    Update xgboost_cv_check.R

--- code-analysis/cv-check/xgboost_cv_check.R
+++ code-analysis/cv-check/xgboost_cv_check.R
@@ -23,3 +23,13 @@ model_xgb <- train(is_duplicate~., data=trainset ,method="xgbTree", trControl= f
 
 # So 5 fold cv =      is equivalent to public LB = 0.151 
 # Let us benchmark this 
+# Accuracy   Kappa      LogLoss  
+#  0.9321653  0.7620084  0.1655568
+
+#Tuning parameter 'nrounds' was held constant at a value of 1000
+#Tuning parameter 'max_depth' was held constant at a
+# a value of 0
+#Tuning parameter 'colsample_bytree' was held constant at a value of 1
+#Tuning parameter
+# 'min_child_weight' was held constant at a value of 1
+#Tuning parameter 'subsample' was held constant at a value of 1
