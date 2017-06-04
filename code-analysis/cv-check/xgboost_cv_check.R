db62a1b14369757cca025966bd07e412aea975e8
Germayne <germayneng@hotmail.com>
2017-06-04 22:14:50 +0800

    Update xgboost_cv_check.R

--- code-analysis/cv-check/xgboost_cv_check.R
+++ code-analysis/cv-check/xgboost_cv_check.R
@@ -6,10 +6,20 @@ source("logloss.R")
 
 set.seed(12357)
 
-fitcontrol <- trainControl(method = "cv", number - 5, savePredictions = "final",classProbs = TRUE, summaryFunction = LogLosSummary)
+fitcontrol <- trainControl(method = "cv", number =5, savePredictions = "final",
+                           classProbs = TRUE, verboseIter = TRUE, summaryFunction = LogLosSummary)
+
+
+
 xgb_grid <- expand.grid(nrounds= 1000, 
                         max_depth= 5 ,eta= 0.1, 
                         gamma= 0, colsample_bytree = c(1), 
                         min_child_weight = c(1), subsample = 1)
 
-model_xgb <- train(is_duplicate~., data=trainset ,method="xgbTree", trControl= fitcontrol, tuneGrid= xgb_grid, metric="LogLoss",maximize = FALSE)
+model_xgb <- train(is_duplicate~., data=trainset ,method="xgbTree", trControl= fitcontrol, 
+                   tuneGrid= xgb_grid, metric="LogLoss",maximize = FALSE, verbose = TRUE)
+
+
+
+# So 5 fold cv =      is equivalent to public LB = 0.151 
+# Let us benchmark this 
